fa818717572ae27176f1b0a1b976e36fab74890e
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-04-17 20:34:17 +0430

    feat: show and calculate income and expense

--- src/components/IncomeExpenseBox.jsx
+++ src/components/IncomeExpenseBox.jsx
@@ -1,16 +1,28 @@
 import { Row, Col } from "react-bootstrap";
+import {useContext} from 'react'
+import { ExpenseTracker } from "../context/ExpenseTrackerContext";
 
 const IncomeExpenceBox = () => {
+
+  const {transactions} = useContext(ExpenseTracker)
+
+  const amounts = transactions.map(tr => tr.amount)
+  const income = amounts.filter(item => item>0)
+  .reduce((acc,item) =>(acc +=item),0)
+
+  const expense = amounts.filter(item => item <0)
+  .reduce((acc,item) => (acc +=item),0) * -1;
+
   return (
     <>
       <Row className=" mx-auto">
         <Col>
           <p>income</p>
-          <p>$00.00</p>
+          <p>${income}</p>
         </Col>
         <Col>
           <p> Expense</p>
-          <p>$00.00</p>
+          <p>${expense}</p>
         </Col>
       </Row>
     </>
